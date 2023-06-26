3ee0e78d7341e79a9d634a5db0a6ae465826e2bd
steff04 <91566451+Steffanoslc@users.noreply.github.com>
2023-06-25 23:03:48 -0400

    About seccion completed

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,24 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { exploreWorlds } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
+
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| The World" textStyles="text-center" />
+      <TitleText title={<>Chose the world you want <br className="md:block hidden" />to explore </>} textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
